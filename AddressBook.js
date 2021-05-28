4effc1036fa32d8697c0d10fa9d6e397441a180b
shruti3105 <82582531+shruti3105@users.noreply.github.com>
2021-05-28 11:42:03 +0530

    Update AddressBook.js

--- AddressBook.js
+++ AddressBook.js
@@ -1,47 +1,94 @@
-//UC1: create a contact
-class Contact {
-  firstName;
-  lastName;
-  address;
-  city;
-  state;
-  zip;
-  phoneNumber;
-  email;
-
-  constructor(
-    firstName,
-    lastName,
-    address,
-    city,
-    state,
-    zip,
-    phoneNumber,
-    email
-  ) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.address = address;
-    this.city = city;
-    this.state = state;
-    this.zip = zip;
-    this.phoneNumber = phoneNumber;
-    this.email = email;
+class AddressBook {
+  //constructor
+  constructor(...params) {
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zip = params[5];
+    this.phoneNumber = params[6];
+    this.email = params[7];
   }
 
+  //get and set for firstname
+  get firstName() {
+    return this._firstName;
+  }
+  set firstName(firstName) {
+    this._firstName = firstName;
+  }
+
+  //get and set for lastname
+  get lastName() {
+    return this._lastName;
+  }
+  set lastName(lastName) {
+    this._lastName = lastName;
+  }
+
+  //get and set for address
+  get address() {
+    return this._address;
+  }
+  set address(address) {
+    this._address = address;
+  }
+
+  //get and set for city
+  get city() {
+    return this._city;
+  }
+  set city(city) {
+    this._city = city;
+  }
+
+  //get and set for state
+  get state() {
+    return this._state;
+  }
+  set state(state) {
+    this._state = state;
+  }
+
+  //get and set for zip
+  get zip() {
+    return this._zip;
+  }
+  set zip(zip) {
+    this._zip = zip;
+  }
+
+  //get and set for phoneNumber
+  get phoneNumber() {
+    return this._phoneNumber;
+  }
+  set phoneNumber(phoneNumber) {
+    this._phoneNumber = phoneNumber;
+  }
+
+  // get and set for email
+  get email() {
+    return this._email;
+  }
+  set email(email) {
+    this._email = email;
+  }
+
+  //defining to string method
   toString() {
     return (
-      "Firstname: " +
+      "First Name: " +
       this.firstName +
-      ", Lastname: " +
+      ", Last Name: " +
       this.lastName +
-      ", Address: " +
+      "\nAddress: " +
       this.address +
       ", City: " +
       this.city +
       ", State: " +
       this.state +
-      ", Zip: " +
+      "\nZip: " +
       this.zip +
       ", Phone Number: " +
       this.phoneNumber +
@@ -51,7 +98,8 @@ class Contact {
   }
 }
 
-let person = new Contact(
+//creating an instance and giving contact details
+let contact = new AddressBook(
   "Shruti",
   "Verma",
   "Awas Vikas-1",
@@ -61,4 +109,4 @@ let person = new Contact(
   7376563320,
   "shrutiverma3105@gmail.com"
 );
-console.log(person.toString());
+console.log(contact.toString());
